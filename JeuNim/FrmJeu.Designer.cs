47372e39545c3601e4677866fad388faab1e08c7
Nolan C <nolan.collard0803@gmail.com>
2023-12-21 14:15:23 +0100

    testt

--- JeuNim/FrmJeu.Designer.cs
+++ JeuNim/FrmJeu.Designer.cs
@@ -46,40 +46,50 @@
             // labelNombreBaton
             // 
             labelNombreBaton.AutoSize = true;
-            labelNombreBaton.Location = new Point(165, 123);
+            labelNombreBaton.Font = new Font("Showcard Gothic", 19.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            labelNombreBaton.Location = new Point(490, 88);
+            labelNombreBaton.MaximumSize = new Size(200, 150);
             labelNombreBaton.Name = "labelNombreBaton";
-            labelNombreBaton.Size = new Size(106, 15);
+            labelNombreBaton.Size = new Size(173, 129);
             labelNombreBaton.TabIndex = 1;
             labelNombreBaton.Text = "Nombre de bâtons";
+            labelNombreBaton.TextAlign = ContentAlignment.TopCenter;
             // 
             // buttonEnleverBaton
             // 
+            buttonEnleverBaton.BackColor = Color.FromArgb(128, 64, 0);
             buttonEnleverBaton.Enabled = false;
-            buttonEnleverBaton.Location = new Point(146, 256);
+            buttonEnleverBaton.ForeColor = Color.Transparent;
+            buttonEnleverBaton.Location = new Point(490, 451);
+            buttonEnleverBaton.Margin = new Padding(3, 4, 3, 4);
             buttonEnleverBaton.Name = "buttonEnleverBaton";
-            buttonEnleverBaton.Size = new Size(146, 52);
+            buttonEnleverBaton.Size = new Size(167, 69);
             buttonEnleverBaton.TabIndex = 2;
             buttonEnleverBaton.Text = "Enlever";
-            buttonEnleverBaton.UseVisualStyleBackColor = true;
+            buttonEnleverBaton.UseVisualStyleBackColor = false;
             buttonEnleverBaton.Click += buttonEnleverBaton_Click;
             // 
             // labelBatonsAEnlever
             // 
             labelBatonsAEnlever.AutoSize = true;
-            labelBatonsAEnlever.Location = new Point(143, 209);
+            labelBatonsAEnlever.BackColor = Color.Transparent;
+            labelBatonsAEnlever.Font = new Font("Showcard Gothic", 10.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            labelBatonsAEnlever.ForeColor = SystemColors.ControlLightLight;
+            labelBatonsAEnlever.Location = new Point(425, 325);
             labelBatonsAEnlever.Name = "labelBatonsAEnlever";
-            labelBatonsAEnlever.Size = new Size(149, 15);
+            labelBatonsAEnlever.Size = new Size(301, 23);
             labelBatonsAEnlever.TabIndex = 3;
             labelBatonsAEnlever.Text = "Nombre de bâtons à retirer";
             // 
             // numericBatonARetirer
             // 
             numericBatonARetirer.Enabled = false;
-            numericBatonARetirer.Location = new Point(146, 227);
+            numericBatonARetirer.Location = new Point(490, 377);
+            numericBatonARetirer.Margin = new Padding(3, 4, 3, 4);
             numericBatonARetirer.Maximum = new decimal(new int[] { 3, 0, 0, 0 });
             numericBatonARetirer.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numericBatonARetirer.Name = "numericBatonARetirer";
-            numericBatonARetirer.Size = new Size(146, 23);
+            numericBatonARetirer.Size = new Size(167, 27);
             numericBatonARetirer.TabIndex = 4;
             numericBatonARetirer.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
@@ -90,65 +100,74 @@
             // 
             // buttonAbandonner
             // 
-            buttonAbandonner.Location = new Point(339, 276);
+            buttonAbandonner.BackColor = Color.FromArgb(128, 64, 0);
+            buttonAbandonner.ForeColor = Color.White;
+            buttonAbandonner.Location = new Point(940, 13);
+            buttonAbandonner.Margin = new Padding(3, 4, 3, 4);
             buttonAbandonner.Name = "buttonAbandonner";
-            buttonAbandonner.Size = new Size(93, 32);
+            buttonAbandonner.Size = new Size(148, 78);
             buttonAbandonner.TabIndex = 5;
             buttonAbandonner.Text = "Abandonner";
-            buttonAbandonner.UseVisualStyleBackColor = true;
+            buttonAbandonner.UseVisualStyleBackColor = false;
             buttonAbandonner.Click += buttonAbandonner_Click;
             // 
             // labelAdversaire
             // 
             labelAdversaire.AutoSize = true;
-            labelAdversaire.Location = new Point(12, 29);
+            labelAdversaire.Location = new Point(388, 42);
             labelAdversaire.Name = "labelAdversaire";
-            labelAdversaire.Size = new Size(68, 15);
+            labelAdversaire.Size = new Size(86, 20);
             labelAdversaire.TabIndex = 6;
             labelAdversaire.Text = "Adversaire :";
             // 
             // labelNomAdversaire
             // 
             labelNomAdversaire.AutoSize = true;
-            labelNomAdversaire.Location = new Point(124, 29);
+            labelNomAdversaire.Location = new Point(480, 42);
             labelNomAdversaire.Name = "labelNomAdversaire";
-            labelNomAdversaire.Size = new Size(155, 15);
+            labelNomAdversaire.Size = new Size(193, 20);
             labelNomAdversaire.TabIndex = 7;
             labelNomAdversaire.Text = "En attente d'un adversaire ...";
             // 
             // labelNumPar
             // 
             labelNumPar.AutoSize = true;
-            labelNumPar.Location = new Point(12, 6);
+            labelNumPar.Font = new Font("Showcard Gothic", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            labelNumPar.Location = new Point(14, 8);
             labelNumPar.Name = "labelNumPar";
-            labelNumPar.Size = new Size(106, 15);
+            labelNumPar.Size = new Size(158, 18);
             labelNumPar.TabIndex = 8;
             labelNumPar.Text = "Numéro de partie :";
             // 
             // labelNumeroPartie
             // 
             labelNumeroPartie.AutoSize = true;
-            labelNumeroPartie.Location = new Point(124, 6);
+            labelNumeroPartie.Font = new Font("Showcard Gothic", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            labelNumeroPartie.Location = new Point(178, 8);
             labelNumeroPartie.Name = "labelNumeroPartie";
-            labelNumeroPartie.Size = new Size(46, 15);
+            labelNumeroPartie.Size = new Size(70, 18);
             labelNumeroPartie.TabIndex = 9;
             labelNumeroPartie.Text = "NPartie";
             // 
             // buttonQuitter
             // 
-            buttonQuitter.Location = new Point(339, 12);
+            buttonQuitter.BackColor = Color.FromArgb(128, 64, 0);
+            buttonQuitter.ForeColor = Color.White;
+            buttonQuitter.Location = new Point(982, 504);
+            buttonQuitter.Margin = new Padding(3, 4, 3, 4);
             buttonQuitter.Name = "buttonQuitter";
-            buttonQuitter.Size = new Size(93, 32);
+            buttonQuitter.Size = new Size(106, 43);
             buttonQuitter.TabIndex = 10;
             buttonQuitter.Text = "Quitter";
-            buttonQuitter.UseVisualStyleBackColor = true;
+            buttonQuitter.UseVisualStyleBackColor = false;
             buttonQuitter.Click += buttonQuitter_Click;
             // 
             // FrmJeu
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(444, 320);
+            BackgroundImage = Properties.Resources.Jeu_de_nim_2;
+            ClientSize = new Size(1130, 570);
             ControlBox = false;
             Controls.Add(buttonQuitter);
             Controls.Add(labelNumeroPartie);
@@ -160,6 +179,7 @@
             Controls.Add(labelBatonsAEnlever);
             Controls.Add(buttonEnleverBaton);
             Controls.Add(labelNombreBaton);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmJeu";
             Text = "Jeu de Nim";
             ((System.ComponentModel.ISupportInitialize)numericBatonARetirer).EndInit();
