93109e6835a0a6b57e114e2fb485dbae6e4aedc0
Solène <Solne@DESKTOP-N696JIM>
2023-12-21 09:52:21 +0100

    Ajout des models

---
+++ JeuNim/Models/Joueur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JeuNim.Models;
+
+public partial class Joueur
+{
+    public int IdJoueur { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string MotDePasse { get; set; } = null!;
+
+    public virtual ICollection<Participant> Participants { get; set; } = new List<Participant>();
+}
---
+++ JeuNim/Models/Participant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JeuNim.Models;
+
+public partial class Participant
+{
+    public int IdParticipant { get; set; }
+
+    public bool AGagne { get; set; }
+
+    public bool ACommence { get; set; }
+
+    public int IdPartie { get; set; }
+
+    public int IdJoueur { get; set; }
+
+    public virtual ICollection<Coup> Coups { get; set; } = new List<Coup>();
+
+    public virtual Joueur IdJoueurNavigation { get; set; } = null!;
+
+    public virtual Partie IdPartieNavigation { get; set; } = null!;
+}
